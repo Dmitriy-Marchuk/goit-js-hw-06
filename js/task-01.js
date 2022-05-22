c71658a343c5d644fe0efba659424d49e0a1f98e
dmytro-marchuk <darthwudoodoodoo@gmail.com>
2022-05-23 01:12:12 +0300

    Mod-6-task-2

--- js/task-01.js
+++ js/task-01.js
@@ -1,10 +1,11 @@
+// Напиши скрипт который:
 
+// Посчитает и выведет в консоль количество категорий в ul#categories,
+//     то есть элементов li.item.
 
-
-
-
-
-
+// Для каждого элемента li.item в списке ul#categories,
+//     найдет и выведет в консоль текст заголовка элемента(тега < h2 >) 
+//     и количество элементов в категории(всех вложенных в него < li >).
 
 
 const categories = document.querySelectorAll("ul#categories>li.item");
@@ -18,3 +19,4 @@ for (const category of categories) {
 
     console.log(`Category: ${nameCategory} \nElements: ${quantityElements}`);
 };
+
--- js/task-02.js
+++ js/task-02.js
@@ -1,3 +1,15 @@
+// Напиши скрипт, который для каждого элемента массива ingredients:
+
+// Создаст отдельный элемент < li >.Обзательно используй 
+// метод document.createElement().
+
+// Добавит название ингредиента как его текстовое содержимое.
+
+// Добавит элементу класс item.
+
+// После чего вставит все <li> за одну операцию в список ul.ingredients.
+
+
 const ingredients = [
   'Potatoes',
   'Mushrooms',
@@ -6,3 +18,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+for (const ingredient of ingredients) {
+  const li = document.createElement("li");
+  const ul = document.querySelector("ul#ingredients")
+
+  li.textContent = ingredient;
+  li.classList.add("item");
+  ul.append(li);
+
+  console.log(li);
+}
+
+// document.ul#ingredients.append(li);
